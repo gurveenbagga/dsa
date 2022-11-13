8e585966420e8c01cf4e546bbfe709fe99d1aba0
Gurveen Kaur Bagga <49408096+gurveenbagga@users.noreply.github.com>
2022-11-13 22:43:53 +0530

    Understanding Recursion and Memory visualization

---
+++ Recursion/memory-allocation.java
@@ -0,0 +1,19 @@
+// "static void main" must be defined in a public class.
+public class Main {
+    public static void main(String[] args) {
+        int n = 3;
+        PrintFun(n);
+    }
+    
+    static void PrintFun(int n) {
+        if (n < 1) 
+            return;
+        else {
+            System.out.printf("%d ", n);
+            PrintFun(n - 1);
+            System.out.printf("%d ", n);
+            return;
+        }
+    }
+}
+//Refernce - https://www.geeksforgeeks.org/introduction-to-recursion-data-structure-and-algorithm-tutorials/ - to understand how memory is allocated and result returned in recursion;
